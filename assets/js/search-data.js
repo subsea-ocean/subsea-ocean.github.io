24ae2c2ac2fb54e9b9d0fce05e901fbb9d1d4e44
carolynlober <71410675+carolynlober@users.noreply.github.com>
2025-08-27 17:41:17 +0000

    Deploying to gh-pages from @ subsea-ocean/subsea-ocean.github.io@5e78f510f8e5cb335e2e98956bc9f3331a03573d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -93,7 +93,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("//feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
