20ace5ccf64dc443c87885d9710a08aee31a6d6c
Katiebugcoates <katie.coates.um@gmail.com>
2026-05-13 23:23:26 +0000

    Deploying to gh-pages from @ subsea-ocean/subsea-ocean.github.io@ccbb72bfc815dfc14381d7008207ee087ca5b07a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -82,8 +82,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-03-04-subsea-part1-begins.html";
-            },},{id: "news-news-from-the-cruise-subsea-part-1-science-operations-begin",
-          title: 'News from the Cruise: SUBSEA Part 1 Science Operations Begin',
+            },},{id: "news-news-from-the-cruise-s-atlantic-cruise-1-science-operations-begin",
+          title: 'News from the Cruise: S.Atlantic Cruise 1 Science Operations Begin',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-03-07-subsea-part1-station1-operations.html";
